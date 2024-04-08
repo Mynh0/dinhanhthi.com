7eac2fc30c72e467d686e50be30c56d7af029a0a
My Ha Nguyen <158823843+Mynh0@users.noreply.github.com>
2024-04-08 14:32:41 +0700

    socialfix1

--- src/data/social.tsx
+++ src/data/social.tsx
@@ -1,21 +1,8 @@
-import CouseraIcon from '@/public/social/coursera.png'
-import DuolingoIcon from '@/public/social/duolingo.svg'
-import GithubIcon from '@/public/social/github.svg'
-import GoodreadsIcon from '@/public/social/goodreads.svg'
 import LinkedInIcon from '@/public/social/linkedin.svg'
-import Math2ITIcon from '@/public/social/math2it.png'
-import SOIcon from '@/public/social/so.svg'
-import TwitterIcon from '@/public/social/twitter.png'
+import TiktokIcon from 'public/social/tiktok.png'
 import { BadgeSocialProps } from '@notion-x/src/components/BadgeSocial'
 
 const socials: BadgeSocialProps[] = [
-  {
-    id: 'github',
-    title: 'Github',
-    icon: GithubIcon,
-    url: 'https://github.com/dinhanhthi',
-    imgClass: 'invert'
-  },
   {
     id: 'linkedin',
     title: 'LinkedIn',
@@ -23,41 +10,11 @@ const socials: BadgeSocialProps[] = [
     url: 'https://www.linkedin.com/in/dinhanhthi/'
   },
   {
-    id: 'twitter',
-    title: 'Twitter',
-    icon: TwitterIcon,
-    url: 'https://twitter.com/dinhanhthi'
-  },
-  {
-    id: 'goodreads',
-    title: 'Goodreads',
-    icon: GoodreadsIcon,
-    url: 'https://www.goodreads.com/user/show/19630622-thi-dinh'
-  },
-  {
-    id: 'stackexchange',
-    title: 'StackExchange',
-    icon: SOIcon,
-    url: 'https://stackexchange.com/users/1344291/anh-thi-dinh'
-  },
-  {
-    id: 'coursera',
-    title: 'Coursera',
-    icon: CouseraIcon,
-    url: 'https://www.coursera.org/user/47f27dde0ecab9a78e2c93632d78a556'
-  },
-  {
-    id: 'math2it',
-    title: 'Math2IT',
-    icon: Math2ITIcon,
-    url: 'https://math2it.com'
-  },
-  {
-    id: 'duolingo',
-    title: 'Duolingo',
-    icon: DuolingoIcon,
-    url: 'https://www.duolingo.com/profile/mr.Thi'
+    id: 'tiktok',
+    title: 'TikTok',
+    icon: TiktokIconIcon,
+    url: 'https://www.tiktok.com/@mitaplamnguoilon?is_from_webapp=1&sender_device=pc',
   }
-]
+] 
 
 export default socials
