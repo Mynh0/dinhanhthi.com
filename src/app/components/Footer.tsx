e5af1bd185b82c4177970700b3204c7421ecfab6
My Ha Nguyen <158823843+Mynh0@users.noreply.github.com>
2024-04-09 10:54:16 +0700

    footer_change

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -30,14 +30,14 @@ export default function Footer(props: FooterProps) {
           <span>|</span>
           <a
             className={cn(aClass)}
-            href="https://photos.app.goo.gl/9OVEkdTjmtRPg7vC3"
+            href="https://www.linkedin.com/in/myhanguyen07/"
             target="_blank"
           >
-            My sketches <TbExternalLink />
+            Linkedin <TbExternalLink />
           </a>{' '}
           <span>|</span>
-          <a className={cn(aClass)} href="https://goo.gl/photos/yQXdQws1LLS16x5v5" target="_blank">
-            Cooking <TbExternalLink />
+          <a className={cn(aClass)} href="https://www.tiktok.com/@mitaplamnguoilon" target="_blank">
+            TikTok <TbExternalLink />
           </a>{' '}
           <span>|</span>
           <Link className={cn(aClass)} href={'/support-me/'}>
