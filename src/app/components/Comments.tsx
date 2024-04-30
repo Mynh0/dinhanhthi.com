001d2a8dbbab0b6069ebaadf14f359ca82a72730
My Ha Nguyen <158823843+my-ha-nguyen@users.noreply.github.com>
2024-04-30 22:44:05 +0700

    comments_change

--- src/app/components/Comments.tsx
+++ src/app/components/Comments.tsx
@@ -13,7 +13,7 @@ const Comments = ({ className }: { className?: string }) => {
     url: 'https://utteranc.es/client.js',
     theme: 'github-light',
     issueTerm: 'title',
-    repo: 'Mynh0/myweb.com-comments',
+    repo: 'my-ha-nguyen/myweb.com-comments',
     ref: comment
   })
 
